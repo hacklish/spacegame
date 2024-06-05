3704e5b05db5c31b2dce9b21585f8fb2abfcb907
Čeněk Ráliš <c_ralis@utb.cz>
2024-06-05 18:48:54 +0200

    game: fix player ship rotation
    Since on screen coordinates are working a bit differently,
    it is best to rotate sprites 90 degrees, to prevent rotations
    and directional errors, polygon had switched X and Y coordinates
    (plus sign flip due to reverse Y scale on screen).
    
    Ship now aims to mouse cursor nicely

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -7,13 +7,13 @@ var width : int = 3
 
 func _draw():
 	var polygon = [
-					Vector2(0, -30),
-					Vector2(15, -5),
-					Vector2(20, 20),
-					Vector2(0, 0),
+					Vector2(30, 0),
+					Vector2(5, 15),
 					Vector2(-20, 20),
-					Vector2(-15, -5),
-					Vector2(0, -30),
+					Vector2(0, 0),
+					Vector2(-20, -20),
+					Vector2(5, -15),
+					Vector2(30, 0),
 				]
-	
+
 	draw_polyline(polygon, color, width, antialiasing)
