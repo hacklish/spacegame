36cf5693a96c25d6adf04633506be140e90f7170
Čeněk Ráliš <c_ralis@utb.cz>
2024-06-06 00:24:42 +0200

    game: add a enemy
    Enemy folllows player

---
+++ Enemy.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+func _process(delta):
+	look_at(%Player.position)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity = position.direction_to(%Player.position) * SPEED
+
+	move_and_slide()
--- PlayerBody.gd
+++ PlayerBody.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
 var center : Vector2 = Vector2(0, 0)
-var color : Color = Color.BLACK
+var color : Color = Color.WHITE
 var antialiasing : bool = true
 var width : int = 3
 
+var polygon = [
+				Vector2(30, 0),
+				Vector2(5, 15),
+				Vector2(-20, 20),
+				Vector2(0, 0),
+				Vector2(-20, -20),
+				Vector2(5, -15),
+				Vector2(30, 0),
+			]
+
 func _draw():
-	var polygon = [
-					Vector2(30, 0),
-					Vector2(5, 15),
-					Vector2(-20, 20),
-					Vector2(0, 0),
-					Vector2(-20, -20),
-					Vector2(5, -15),
-					Vector2(30, 0),
-				]
+	if not is_in_group("Player"):
+		color = Color.BLACK
 
 	draw_polyline(polygon, color, width, antialiasing)
