e83eb3448dd8dbd8ef96db3fe90d45bda10e43a4
Čeněk Ráliš <c_ralis@utb.cz>
2024-06-05 18:35:58 +0200

    game: add W/S/A/D movement

--- Player.gd
+++ Player.gd
@@ -2,11 +2,22 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+func _process(delta):
 	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
 
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var direction = (mouse_position - position).normalized()
-		velocity = direction * SPEED
+func _physics_process(delta):
+	var direction = Vector2()
+
+	if Input.is_action_pressed("MOVE_UP"):
+		direction += Vector2(0, -1)
+	if Input.is_action_pressed("MOVE_DOWN"):
+		direction += Vector2(0, 1)
+	if Input.is_action_pressed("MOVE_LEFT"):
+		direction += Vector2(-1, 0)
+	if Input.is_action_pressed("MOVE_RIGHT"):
+		direction += Vector2(1, 0)
+
+	velocity = direction * SPEED
 
 	move_and_slide()
